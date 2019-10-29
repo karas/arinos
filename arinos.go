4eee3d7ed329a168276b0699eed6942eedff9e6d
gentom <gento.m.developer@gmail.com>
2019-10-29 23:07:47 +0900

    implement mock StartServe method

--- arinos.go
+++ arinos.go
@@ -1,5 +1,12 @@
 package arinos
 
+import (
+	"fmt"
+	"net/http"
+	"net/http/fcgi"
+	"time"
+)
+
 type Arinos struct {
 	LocalHost bool
 	Options   *Options
@@ -28,4 +35,18 @@ func Port(portNumber int) Option {
 	return func(args *Options) {
 		args.Port = portNumber
 	}
+}
+
+// Mock
+func (arinos *Arinos) StartServe() error {
+	if arinos.LocalHost {
+		portString := fmt.Sprintf(":%d", arinos.Options.Port)
+		server := &http.Server{
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			Addr:         portString,
+		}
+		return server.ListenAndServe()
+	}
+	return fcgi.Serve(nil, nil)
 }
