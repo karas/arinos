4bd8f08cff55f9bf85e8a6f5045e0929bf8c75c6
gentom <gento.m.developer@gmail.com>
2019-10-29 23:00:11 +0900

    add test code [TestNew]

--- arinos_test.go
+++ arinos_test.go
@@ -1,1 +1,21 @@
 package arinos
+
+import (
+	"testing"
+)
+
+func TestNew(t *testing.T) {
+	t.Log("Start")
+	result := New(false)
+	expect := &Arinos{
+		LocalHost: false,
+		Options: &Options{
+			Port: 8000,
+		},
+	}
+	if result.LocalHost != expect.LocalHost &&
+		result.Options.Port != result.Options.Port {
+		t.Error("\nResult： ", result, "\nExpect： ", expect)
+	}
+	t.Log("End")
+}
